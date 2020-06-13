0f0bb782fff2d447d67b9aced0a0b7e0c19bf207
JoelHaubold <joelhaubold@web.de>
2020-06-13 18:17:19 +0200

    Finished automation

--- src/anomaly_thresholds.py
+++ src/anomaly_thresholds.py
@@ -5,11 +5,11 @@ from enum import Enum
 class AThreshold(Enum):
     phase_dif = 7.34
     SeasDif = 5.87
-    SeasDifALt = 3.23
+    SeasDif02 = 3.23
     StationDif = 8.772
-    TimeDif = 179
+    time_passed = 179
     TrafoDif = 0.1 # this low becuase per second
-    TrafoDifAlt = 0.15
+    TrafoDif02 = 0.15
 
 
 if __name__ == '__main__':
--- src/rrcf_anomaly_detection.py
+++ src/rrcf_anomaly_detection.py
@@ -10,8 +10,8 @@ import threading
 from scipy import optimize
 
 
-number_proc = 8
-number_reps = 3
+number_proc = 3
+number_reps = 1
 
 rrcf_cod_directory = Path("../../rrcf_cod")
 rrcf_result_file = Path("./../results/rrcf_results")
@@ -21,9 +21,9 @@ pickle_directory = Path("../../pickles")
 # shingle_size = 0
 # codisp_filename = f"Shingle{shingle_size}_TreeN{num_trees}_TreeS{tree_size}"
 
-num_trees_opt = [100]  #[100, 150, 200]
-tree_size_opt = [10000]#[5000, 10000, 15000, 20000]
-shingle_size_opt = [0] #[0, 3, 5]
+num_trees_opt = [100, 150, 200]
+tree_size_opt = [5000, 10000, 15000, 20000]
+shingle_size_opt = [1, 3, 5]
 
 test_sample_file = Path("../test_sections.csv")
 
@@ -45,29 +45,31 @@ def get_f_value(df_c: DataFrame, rel_cod_threshold, a_threshold: AThreshold):
     return f1, f0_5, f2, precision, recall
 
 
-def analyse_station_phase(df_p, df_constr, tree_size, nmbr_trees, shingle_size):
+def analyse_station_phase(df_p, df_constr, tree_size, nmbr_trees, shingle_size, a_type):
     # sdp_directory = rrcf_directory / station_name / codisp_filename
     # if not os.path.exists(sdp_directory):
     #     os.makedirs(sdp_directory)
 
+    print(df_p)
+    print(df_constr)
+    streaming_points = df_p[a_type.name].values
+    constr_points = df_constr[a_type.name].values
     # Prepare points
-    num_points = df_p.shape[0]
-    print(num_points)
-    if shingle_size != 0:
-        points = rrcf.shingle(df_p, size=shingle_size)
+    if shingle_size != 1:
+        points = rrcf.shingle(streaming_points, size=shingle_size)
         points = np.vstack([point for point in points])
-        num_points = points.shape[0]
-    sample_size_range = (num_points // tree_size, tree_size)
+        streaming_points = points
+        points = rrcf.shingle(constr_points, size=shingle_size)
+        points = np.vstack([point for point in points])
+        constr_points = points
+        # num_points = points.shape[0]
+
+    # sample_size_range = (num_points // tree_size, tree_size)
     forest = []
 
     # Construct forest
     avg_codisp = {}
-    print(df_p)
-    print(df_constr)
-    streaming_points = df_p.to_numpy()
-    constr_points = df_constr.to_numpy()
-    print(streaming_points)
-    print(constr_points)
+
     for _ in range(nmbr_trees):
         tree = rrcf.RCTree()
         forest.append(tree)
@@ -98,26 +100,37 @@ def analyse_station_phase(df_p, df_constr, tree_size, nmbr_trees, shingle_size):
 
     # Save Codisp
     # avg_codisp.to_json(sdp_directory / "codisp")
-    return avg_codisp
+    print(f"length codisp: {len(list(avg_codisp.values()))}")
+    return list(avg_codisp.values())
 
 
-def calc_mcc(df_p, avg_codisp, cod_threshold, a_threshold):
+def calc_mcc(df_points, avg_codisp_a, cod_threshold, a_threshold, shingle_size):
+    df_p = df_points.iloc[shingle_size-1:]
+    avg_codisp = pd.Series(index=df_p.index, data=avg_codisp_a)
     tp = df_p[(avg_codisp >= cod_threshold) & (df_p[a_threshold.name] >= a_threshold.value)].shape[0]
     tn = df_p[(avg_codisp < cod_threshold) & (df_p[a_threshold.name] < a_threshold.value)].shape[0]
-    fp = df_p.shape[0] - tp
-    fn = df_p.shape[0] - tn
-
-    mcc = (tp*tn - fp*fn)/(((tp+fp)(tp+fn)(tn+fp)(tn+fn))**0.5)
-
+    fp = df_p[(avg_codisp >= cod_threshold) & (df_p[a_threshold.name] < a_threshold.value)].shape[0]
+    fn = df_p[(avg_codisp < cod_threshold) & (df_p[a_threshold.name] >= a_threshold.value)].shape[0]
+    if (tp+fp)*(tp+fn)*(tn+fp)*(tn+fn) != 0:
+        mcc = (tp*tn - fp*fn)/(((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))**0.5)
+    else:
+        mcc = tp*tn - fp*fn
+    print(f"mcc:{mcc},thres:{cod_threshold}")
     return mcc
 
 
-def calc_opt_cod_threshold(df_p, avg_codisp, a_threshold):
+thres_opt = np.arange(10, 600 ,10)
 
-    scipy_result = optimize.fmin(lambda x: -calc_mcc(df_p, avg_codisp, x, a_threshold), 100)
-    opt_mcc = scipy_result.fopt
-    cod_thresh = scipy_result.xopt
-    return cod_thresh, opt_mcc
+
+def calc_opt_cod_threshold(df_p, avg_codisp, a_threshold, shingle_size):
+    calculated_mcc = {}
+    max_mcc = -1
+    for to in thres_opt:
+        mcco = calc_mcc(df_p, avg_codisp, to, a_threshold, shingle_size)
+        calculated_mcc[mcco] = to
+        max_mcc = max(max_mcc, mcco)
+    # cod_thresh, opt_mcc = optimize.fmin(func=lambda x: -calc_mcc(df_p, avg_codisp, int(x), a_threshold), x0=20)
+    return calculated_mcc[max_mcc], max_mcc
 
 
 lock = threading.Lock()
@@ -125,26 +138,34 @@ lock = threading.Lock()
 
 def save_result(station, start_date, end_date, tree_size, nmbr_trees, shingle_size, cod_threshold, mcc, calc_nmbr):
     with lock:
-        df = pd.read_pickle(rrcf_result_file)
-        df = df.append({'station': station,'start_date': start_date,'end_date': end_date,'tree_size': tree_size,
+        if rrcf_result_file.exists():
+            df = pd.read_pickle(rrcf_result_file)
+        else:
+            df = pd.DataFrame(columns=['station', 'start_date', 'end_date', 'tree_size', 'nmbr_trees', 'shingle_size',
+                                       'cod_threshold', 'mcc', 'calc_nmbr'])
+        df = df.append({'station': station,'start_date': start_date,'end_date': end_date, 'tree_size': tree_size,
                         'nmbr_trees': nmbr_trees, 'shingle_size': shingle_size, 'cod_threshold': cod_threshold,
                         'mcc': mcc, 'calc_nmbr': calc_nmbr}, ignore_index=True)
         df.to_pickle(rrcf_result_file)
 
 
 def run_hp_test(df_constr, df_p, station, start_date, end_date, tree_size, nmbr_trees, shingle_size, a_type, calc_nmbr):
-    avg_codisp = analyse_station_phase(df_p, df_constr, tree_size, nmbr_trees, shingle_size)
-    np.save(arr=avg_codisp, file=rrcf_cod_directory / f"{station}_{start_date}_{end_date}_{tree_size}_{nmbr_trees}_{shingle_size}_{a_type.name}_{calc_nmbr}")
+    print("dddd")
+    # avg_codisp = np.load(file=rrcf_cod_directory / "512333.npy", allow_pickle=True)
+    # print(avg_codisp)
+    # cod_threshold, mcc = calc_opt_cod_threshold(df_p, avg_codisp, a_type)
+    avg_codisp = analyse_station_phase(df_p, df_constr, tree_size, nmbr_trees, shingle_size, a_type)
+    np.save(arr=avg_codisp, file=rrcf_cod_directory / str(calc_nmbr))
 
-    cod_threshold, mcc = calc_opt_cod_threshold(df_p, avg_codisp, a_type)
+    cod_threshold, mcc = calc_opt_cod_threshold(df_p, avg_codisp, a_type, shingle_size)
 
     save_result(station, start_date, end_date, tree_size, nmbr_trees, shingle_size, cod_threshold, mcc, calc_nmbr)
 
 
-def get_test_sample(station, start, end, a_type, tree_size):
+def get_test_sample(station, start, end, a_type, tree_size, shingle_size):
     f_path = pickle_directory / station
     df_phases = list(
-        map(lambda p: pd.read_pickle(f_path / ("h_phase" + p)).loc[:, ['Value', 'phase_dif']], ['1', '2', '3']))
+        map(lambda p: pd.read_pickle(f_path / ("h_phase" + p)), ['1', '2', '3']))
     df_p = df_phases[0].rename(columns={"Value": "p1"})
     df_p['p2'] = df_phases[1].Value
     df_p['p3'] = df_phases[2].Value
@@ -152,9 +173,10 @@ def get_test_sample(station, start, end, a_type, tree_size):
     df_p = df_p[[a_type.name]]
 
     start_row = df_p.index.get_loc(start, method='backfill').start
-    df_const = df_p.iloc[(start_row - tree_size):start_row]
+    end_row = df_p.index.get_loc(end, method='pad').start
+    df_const = df_p.iloc[(start_row - tree_size - shingle_size + 1):start_row]
 
-    df_p = df_p[start:end]
+    df_p = df_p.iloc[(start_row - shingle_size + 1):end_row+1]
 
     return df_p, df_const
 
@@ -165,7 +187,7 @@ running_procs = []
 def start_process(p):
     i = 0
     while len(running_procs) >= number_proc:
-        if i <= 8:
+        if i < number_proc:
             ex_code = running_procs[i].join(1)
             if ex_code is not None:
                 del running_procs[i]
@@ -185,17 +207,21 @@ def test_h_parameters():
         for ts in tree_size_opt:
             for tn in num_trees_opt:
                 for ss in shingle_size_opt:
+                    print('x')
                     station = row['station']
                     start = row['start']
                     end = row['end']
                     a_type = row['a_type']
                     a_type = AThreshold[a_type]
-                    df_p, df_const = get_test_sample(station, start, end, a_type, ts)
-
+                    df_p, df_const = get_test_sample(station, start, end, a_type, ts, ss)
+                    print("y")
                     for pn in range(number_reps):
+                        print(pn)
                         calc_nmbr = np.random.randint(low=0, high=999999)
                         p = Process(target=run_hp_test, args=(df_const, df_p, station, start, end, ts, tn, ss, a_type, calc_nmbr))
                         start_process(p)
+    for rp in running_procs:
+        rp.join()
 
 
 def main():

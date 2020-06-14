3e82c28b581a25f08a4c70c20722a29ceba422b4
Joel Haubold <joelhaubold@web.de>
2020-06-14 14:25:15 +0200

    Differentiating bewtween phases

--- src/anomaly_thresholds.py
+++ src/anomaly_thresholds.py
@@ -8,8 +8,8 @@ class AThreshold(Enum):
     SeasDif02 = 3.23
     StationDif = 8.772
     time_passed = 179
-    TrafoDif = 0.1 # this low becuase per second
-    TrafoDif02 = 0.15
+    trafo = 0.1 # this low becuase per second
+    trafo02 = 0.15
 
 
 if __name__ == '__main__':
